ab200dc29a0775f3c6cc1166a5d9a24b0177bd6b
Mizux Seiha <mizux.dev@gmail.com>
2022-02-23 05:46:02 +0100

    ci: Fix sample.py

--- ci/samples/sample.py
+++ ci/samples/sample.py
@@ -1,16 +1,13 @@
-import pythonnative
-from pythonnative.foo import pyFoo
-from pythonnative.bar import pyBar
-from pythonnative.foobar import pyFooBar
+import cmakepybind11
+from cmakepybind11.foo import pyFoo
+from cmakepybind11.bar import pyBar
+from cmakepybind11.foobar import pyFooBar
 
-print(f'version: {pythonnative.__version__}')
+print(f'version: {cmakepybind11.__version__}')
 
 # foo
 print(f'Foo: {dir(pyFoo.Foo)}')
 
-p = pyFoo.IntPair(3, 5)
-print(f"class IntPair: {dir(p)}")
-print(f"p: {p}")
 pyFoo.free_function(2147483647) # max int
 pyFoo.free_function(2147483647+1) # max int + 1
 
@@ -19,17 +16,14 @@ print(f'class Foo: {dir(f)}')
 f.static_function(1)
 f.static_function(2147483647)
 f.static_function(2147483647+1)
-f.set_int(13)
-assert(f.get_int() == 13)
-f.set_int64(31)
-assert(f.get_int64() == 31)
+f.int = 13
+assert(f.int == 13)
+f.int64 = 31
+assert(f.int64 == 31)
 
 # bar
 print(f'Bar: {dir(pyBar.Bar)}')
 
-p = pyBar.IntPair(3, 5)
-print(f"class IntPair: {dir(p)}")
-print(f"p: {p}")
 pyBar.free_function(2147483647) # max int
 pyBar.free_function(2147483647+1) # max int + 1
 
@@ -38,17 +32,14 @@ print(f'class Bar: {dir(b)}')
 b.static_function(1)
 b.static_function(2147483647)
 b.static_function(2147483647+1)
-b.set_int(13)
-assert(b.get_int() == 13)
-b.set_int64(31)
-assert(b.get_int64() == 31)
+b.int = 13
+assert(b.int == 13)
+b.int64 = 31
+assert(b.int64 == 31)
 
 # foobar
 print(f'FooBar: {dir(pyFooBar.FooBar)}')
 
-p = pyFooBar.IntPair(3, 5)
-print(f"class IntPair: {dir(p)}")
-print(f"p: {p}")
 pyFooBar.free_function(2147483647) # max int
 pyFooBar.free_function(2147483647+1) # max int + 1
 
@@ -57,9 +48,9 @@ print(f'class FooBar: {dir(fb)}')
 fb.static_function(1)
 fb.static_function(2147483647)
 fb.static_function(2147483647+1)
-fb.set_foo_int(13)
-fb.set_bar_int(17)
-assert(fb.get_int() == 30)
-fb.set_foo_int64(31)
-fb.set_bar_int64(37)
-assert(fb.get_int64() == 68)
+fb.foo_int = 13
+fb.bar_int = 17
+assert(fb.int == 30)
+fb.foo_int64 = 31
+fb.bar_int64 = 37
+assert(fb.int64 == 68)
